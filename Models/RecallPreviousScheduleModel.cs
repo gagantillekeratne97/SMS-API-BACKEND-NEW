11232cfbf5414e6efb9e6f51f18db0a8481006f6
Gagan Tillekeratne <110877796+gagantillekeratne97@users.noreply.github.com>
2026-02-05 10:04:22 +0530

    schedule recall function

---
+++ Models/RecallPreviousScheduleModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ServvistaWebAppAPI.Models
+{
+    public class RecallPreviousScheduleModel
+    {        
+        public string recallReason { get; set; }
+        public DateTime recallDate { get; set; }
+        public int rowID { get; set; }
+        public int visitNo { get; set; }
+        public bool isRecall { get; set; }
+        public bool onSite { get; set; }
+    }
+}
