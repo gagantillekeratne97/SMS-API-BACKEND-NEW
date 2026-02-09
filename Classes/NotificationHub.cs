3da84737e28fb25a91f69cd60c96208c618253b3
Gagan Tillekeratne <110877796+gagantillekeratne97@users.noreply.github.com>
2026-02-09 09:29:31 +0530

    notification features added

--- Classes/NotificationHub.cs
+++ Classes/NotificationHub.cs
@@ -1,8 +1,19 @@
-﻿using Microsoft.AspNetCore.SignalR; 
+﻿using Microsoft.AspNetCore.SignalR;
 namespace ServvistaWebAppAPI.Classes
 {
     public class NotificationHub : Hub
     {
-        
+        public override async Task OnConnectedAsync()
+        {
+            var httpContext = Context.GetHttpContext();
+            var techCode = httpContext.Request.Query["techCode"].ToString();
+
+            if (!string.IsNullOrEmpty(techCode))
+            {
+                await Groups.AddToGroupAsync(Context.ConnectionId, techCode);
+            }
+
+            await base.OnConnectedAsync();
+        }
     }
 }
